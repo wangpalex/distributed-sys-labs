3bc9f21c9bebb8acb323ea037d50b348630d387c
wangpalex <alex4135qwq@gmail.com>
2023-04-14 17:28:02 +0800

    Add mass test config for 2D

---
+++ src/raft/mass_test_2D.sh
@@ -0,0 +1,8 @@
+./dstest.py \
+TestSnapshotBasic2D TestSnapshotInstall2D TestSnapshotInstallUnreliable2D TestSnapshotInstallCrash2D \
+TestSnapshotInstallUnCrash2D TestSnapshotAllCrash2D TestSnapshotInit2D \
+-p 20 \
+-n 1000 \
+-o ./test_logs \
+-R \
+-v
