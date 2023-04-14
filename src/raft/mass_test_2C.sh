330e1b8af4bb17fb82c16b704d37c2f7a29e979c
wangpalex <alex4135qwq@gmail.com>
2023-04-14 22:51:05 +0800

    Modify 2C test

--- src/raft/mass_test_2C.sh
+++ src/raft/mass_test_2C.sh
@@ -1,7 +1,5 @@
 ./dstest.py \
-TestPersist12C TestPersist22C TestPersist32C \
-TestFigure82C TestUnreliableAgree2C TestFigure8Unreliable2C \
-internalChurn TestReliableChurn2C TestUnreliableChurn2C \
+TestFigure8Unreliable2C \
 -p 20 \
 -n 1000 \
 -o ./saved_2C_logs \
